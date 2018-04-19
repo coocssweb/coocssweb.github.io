149d762723a5a62195b7941f66881ff189eeef4c
coocssweb <1974740999@qq.com>
2018-04-19 22:30:52 +0800

    remove source

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './scss/home.scss';
-import App from './app';
-import router from './router';
-import Loading from './modules/loading';
-
-this._app = new App({router});
-this._app._router.init();
