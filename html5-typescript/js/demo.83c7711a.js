65777eed93e25b7ee2a259e4c04d6be27d8a401e
coocssweb <1974740999@qq.com>
2019-04-28 13:56:49 +0800

    publish html-typscript

---
+++ html5-typescript/js/demo.83c7711a.js
@@ -0,0 +1,5 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["demo"],{CwpX:function(t,s,a){"use strict";a.r(s);a("OebX");var e=a("9cwZ"),i=a("9W30");new e.a({data:{},watchs:{},bindEvents(){},init(){new i.a(["http://assets.shuxia123.com/uploads/2019/1555171314834_width_1289_height_476.png","http://assets.shuxia123.com/uploads/2019/1555170559378_width_800_height_349.png","http://assets.shuxia123.com/uploads/2019/1554905994308_width_500_height_350.jpeg"],()=>{console.log("图片加完成")}).start()}})},OebX:function(t,s,a){}},[["CwpX","manifest","app","common"]]]);
+/**
+ * 作者: 王佳欣
+ * 站点: http://www.shuxia123.com
+ */
